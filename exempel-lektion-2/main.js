95460a03f5cd5d518ce343904ce3006f8223df58
Kasper Tengvall <tengvalk@arcada.fi>
2023-10-31 11:58:31 +0200

    Add files for lesson 2

---
+++ exempel-lektion-2/main.js
@@ -0,0 +1,17 @@
+const namn = "Javascript";
+const version = 1.0;
+
+const str1 = "Hello " + namn + " version " + version;
+
+function hello() {
+
+    const str2 = `Hello function`;
+    console.log(str2);
+}
+
+function btnHandler() {
+    console.log("Button has been pressed");
+}
+
+console.log(str1);
+
